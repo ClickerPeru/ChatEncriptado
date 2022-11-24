e5e6a77d11849cb4d90292c78cff088dcd2b762e
Jonathan <jonathan.prieto@me.com>
2022-11-24 07:54:33 -0500

    Remove unused dependencies in urls.py

--- ChatEncriptado/urls.py
+++ ChatEncriptado/urls.py
@@ -15,8 +15,6 @@ Including another URLconf
 """
 from django.urls import path, include, re_path
 from django.contrib import admin
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 
 admin.site.site_header = "Encripted Chat Stracontech"
