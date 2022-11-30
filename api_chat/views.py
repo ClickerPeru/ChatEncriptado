ee3151e300f710047e0147e11e7133c81309b1c0
Jonathan <jonathan.prieto@me.com>
2022-11-29 23:01:19 -0500

    Cuando creas chat pasar el nombre
    te paso el id de la persona cuando creas el chat
    
    te paso el nombre cuando te logeas

--- api_chat/views.py
+++ api_chat/views.py
@@ -16,10 +16,76 @@ from api_chat.get_token import get_user_from_token
 from rest_framework.views import APIView
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.settings import api_settings
+from knox.settings import knox_settings
+from django.utils import timezone
+from knox.models import AuthToken
+from django.contrib.auth.signals import user_logged_in
+from rest_framework.serializers import DateTimeField
 
+class LoginView(APIView):
+    authentication_classes = api_settings.DEFAULT_AUTHENTICATION_CLASSES
+    permission_classes = (IsAuthenticated,)
+
+    def get_context(self):
+        return {'request': self.request, 'format': self.format_kwarg, 'view': self}
+
+    def get_token_ttl(self):
+        return knox_settings.TOKEN_TTL
+
+    def get_token_limit_per_user(self):
+        return knox_settings.TOKEN_LIMIT_PER_USER
+
+    def get_user_serializer_class(self):
+        return knox_settings.USER_SERIALIZER
+
+    def get_expiry_datetime_format(self):
+        return knox_settings.EXPIRY_DATETIME_FORMAT
+
+    def format_expiry_datetime(self, expiry):
+        datetime_format = self.get_expiry_datetime_format()
+        return DateTimeField(format=datetime_format).to_representation(expiry)
+
+    def get_post_response_data(self, request, token, instance):
+        UserSerializer = self.get_user_serializer_class()
+        if UserSerializer is not None:
+            data = {
+                'expiry': self.format_expiry_datetime(instance.expiry),
+                'token': token,
+                'user_name': request.user.name
+            }
+        else:
+            data = {
+                'expiry': self.format_expiry_datetime(instance.expiry),
+                'token': token
+            }
+        if UserSerializer is not None:
+            data["user"] = UserSerializer(
+                request.user,
+                context=self.get_context()
+            ).data
+        return data
+
+    def post(self, request, format=None):
+        token_limit_per_user = self.get_token_limit_per_user()
+        if token_limit_per_user is not None:
+            now = timezone.now()
+            token = request.user.auth_token_set.filter(expiry__gt=now)
+            if token.count() >= token_limit_per_user:
+                return Response(
+                    {"error": "Maximum amount of tokens allowed per user exceeded."},
+                    status=status.HTTP_403_FORBIDDEN
+                )
+        token_ttl = self.get_token_ttl()
+        instance, token = AuthToken.objects.create(request.user, token_ttl)
+        user_logged_in.send(sender=request.user.__class__,
+                            request=request, user=request.user)
+        data = self.get_post_response_data(request, token, instance)
+        return Response(data)
 
 @method_decorator(csrf_exempt, name='dispatch')
-class LoginAPI(KnoxLoginView):
+class LoginAPI(LoginView):
     permission_classes = (permissions.AllowAny,)
 
     def post(self, request, format=None):
@@ -35,6 +101,7 @@ class LoginAPI(KnoxLoginView):
             user.save()
 
         login(request, user)
+        print(request.user.name)
         return super().post(request, format=None)
 
 @method_decorator(csrf_exempt, name='dispatch')
@@ -479,7 +546,9 @@ class CreateChat(APIView):
         return Response({
             'status': True,
             'detail': 'El Chat ha sido creado satisfactoriamente.',
-            'id_conversacion': str(new_chat.pk)
+            'id_conversacion': str(new_chat.pk),
+            'id_destinatario': str(new_chat.user_hasta.pk),
+            'nombre_destinatario': str(new_chat.user_hasta.name)
         })
 
 @method_decorator(csrf_exempt, name='dispatch')
